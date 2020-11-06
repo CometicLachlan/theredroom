e8dcc185b5923f242096cc6c8196507d9aa6760b
CometicLachlan <66243601+CometicLachlan@users.noreply.github.com>
2020-11-06 20:44:17 +1100

    Did A Whole Lotta Stuff

---
+++ The Red Room/Assets/CameraMovement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    public Transform Player;
+
+    void Update() {
+        transform.position = Player.transform.position;
+    }
+}
