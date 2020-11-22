9368317913e67232308f47d49a85161fc6e1c375
CometicLachlan <66243601+CometicLachlan@users.noreply.github.com>
2020-11-22 11:41:07 +1100

    Updated PlayerMovement Script

--- The Red Room/Assets/Scripts/PlayerMovement.cs
+++ The Red Room/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class Example : MonoBehaviour
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
+    //private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
     private void Start()
@@ -33,10 +33,10 @@ public class Example : MonoBehaviour
         }
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
+       // if (Input.GetButtonDown("Jump") && groundedPlayer)
+        //{
+          //  playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        //}
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
