0ec61cf27281b14b628aed9d7b7004a23b3558e5
CometicLachlan <66243601+CometicLachlan@users.noreply.github.com>
2020-12-22 18:16:58 +1100

    Umm, Yeah Just, May Or MAy Not Have Deleted And Added A Main Menu
    YEET

---
+++ The Red Room/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour{
+
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("Right Now, The Game Would Have Closed, But We Are In The Unity Engine, So The Game Does Not Close");
+        Application.Quit();
+    }
+
+}
+
