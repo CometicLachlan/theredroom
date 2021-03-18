7a6006cf8281fcc2d615b865ba3e857432b66383
CometicLachlan <66243601+CometicLachlan@users.noreply.github.com>
2021-03-18 18:47:02 +1100

    added a bad tp script that does not work

---
+++ The Red Room/Assets/TeleportationScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportationScript : MonoBehaviour
+{
+    public Transform Teleport;
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(-884.438, -438.829, -57.7, 95.61768), "BasePlate"))
+        {
+            transform.position = Teleport.position;
+        }
+    }
+}
