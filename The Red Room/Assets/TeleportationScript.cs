caf861ac2c049d89958753394a144e8b48b9edb3
cuzwhy <74037281+cuzwhy@users.noreply.github.com>
2021-03-18 19:37:38 +1100

    YAYY

--- The Red Room/Assets/TeleportationScript.cs
+++ The Red Room/Assets/TeleportationScript.cs
@@ -8,7 +8,7 @@ public class TeleportationScript : MonoBehaviour
 
     private void OnGUI()
     {
-        if(GUI.Button(new Rect(-884.438, -438.829, -57.7, 95.61768), "BasePlate"))
+        if(GUI.Button(new Rect((float)-884.438, (float) -438.829, (float) -57.7, (float)95.61768), "BasePlate"))
         {
             transform.position = Teleport.position;
         }
